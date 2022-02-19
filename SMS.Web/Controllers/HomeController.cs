4efece12963c9ec3ce84bc93ab5f11abe1711905
SeanGormley <99802537+gormley-s20@users.noreply.github.com>
2022-02-19 17:26:44 +0000

    After review

--- SMS.Web/Controllers/HomeController.cs
+++ SMS.Web/Controllers/HomeController.cs
@@ -6,41 +6,34 @@ namespace SMS.Web.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index(){
+    public IActionResult Index()
+    {
         
       DateTime date_Time = DateTime.Now;
-      
       var longTime = date_Time.ToLongTimeString();
-
       var message = "Time Now";
 
       ViewBag.LongTime = longTime;
       ViewBag.Message = message;
  
       return View();
-
     }
 
-    public IActionResult About(){
-       
-      
-       // construct the view model
-       var about = new AboutViewModel 
-       {
-       Formed = new DateTime (2020, 1, 1),
-       CompanyName = "Googazon"
-       };    
-   // render the view
-   return View(about);
-
+    public IActionResult About()
+    {
+        // construct the view model
+        var about = new AboutViewModel 
+        {
+            Formed = new DateTime (2020, 1, 1),
+            CompanyName = "Googazon"
+        };    
+        // render the view
+        return View(about);
     }
-
-    
 
     public IActionResult Privacy()
     {
         return View();
-
     }
 }
 
